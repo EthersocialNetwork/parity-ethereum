6df67ae43d47375ee8f0c3a56efaf159ca1f1a53
hackyminer <hackyminer@gmail.com>
2018-03-21 23:31:24 +0900

    Support Ethersocial Network
     * EIP155 + EIP158 at 845,000
     * fixed genesis json for parity 2.x

--- ethcore/src/ethereum/mod.rs
+++ ethcore/src/ethereum/mod.rs
@@ -114,6 +114,11 @@ pub fn new_callisto<'a, T: Into<SpecParams<'a>>>(params: T) -> Spec {
 	load(params.into(), include_bytes!("../../res/ethereum/callisto.json"))
 }
 
+/// Create a new EtherSocial chain spec.
+pub fn new_ethersocial<'a, T: Into<SpecParams<'a>>>(params: T) -> Spec {
+	load(params.into(), include_bytes!("../../res/ethereum/ethersocial.json"))
+}
+
 // For tests
 
 /// Create a new Foundation Frontier-era chain spec as though it never changes to Homestead.
